f0c160aba36e61e2595f93096cbc8756a76489e1
Jeremy Djohar Riyadi <162307355+JeremyRiyadi@users.noreply.github.com>
2025-09-03 15:50:22 +0700

    Add files via upload

---
+++ SoulcastStoreProductDetails/script.js
@@ -0,0 +1,7 @@
+document.querySelector('#back-button').addEventListener("click", () => {
+  window.location.href = "../SoulcastStore-Main/index.html"
+})
+
+document.querySelector('#cart').addEventListener("click", () => {
+  window.location.href = "../SoulcastStore-CartCheckout/index.html"
+})
