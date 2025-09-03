0dfe393eaadea0b27e89b915242f3fac0f584675
Jeremy Djohar Riyadi <162307355+JeremyRiyadi@users.noreply.github.com>
2025-09-03 15:55:03 +0700

    Add files via upload

---
+++ WhisperingSoul-detail/whisperingSoull.js
@@ -0,0 +1,492 @@
+const popular_day = [
+    {
+        image: "/image/WhisperingSoul/mentalhealth.png",
+        title: "#mentalhealth",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/Anxiety.png",
+        title: "#anxiety",
+        subtitle: "65,523 Posted &#8226 Trending",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/selfcare.png",
+        title: "#selfcare",
+        subtitle: "51,354 Trending &#8226 in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/mindfullness.png",
+        title: "#mindfulness",
+        subtitle: "48,029 Posted by this tag",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/therapy.png",
+        title: "#therapy",
+        subtitle: "51,354 &#8226 Trending in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/wellbeing.png",
+        title: "#wellbeing",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/mentalhealth.png",
+        title: "#mentalhealth",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/Anxiety.png",
+        title: "#anxiety",
+        subtitle: "65,523 Posted &#8226 Trending",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/selfcare.png",
+        title: "#selfcare",
+        subtitle: "51,354 Trending &#8226 in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/mindfullness.png",
+        title: "#mindfulness",
+        subtitle: "48,029 Posted by this tag",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/therapy.png",
+        title: "#therapy",
+        subtitle: "51,354 &#8226 Trending in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/wellbeing.png",
+        title: "#wellbeing",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/mentalhealth.png",
+        title: "#mentalhealth",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/Anxiety.png",
+        title: "#anxiety",
+        subtitle: "65,523 Posted &#8226 Trending",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/selfcare.png",
+        title: "#selfcare",
+        subtitle: "51,354 Trending &#8226 in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/mindfullness.png",
+        title: "#mindfulness",
+        subtitle: "48,029 Posted by this tag",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/therapy.png",
+        title: "#therapy",
+        subtitle: "51,354 &#8226 Trending in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/wellbeing.png",
+        title: "#wellbeing",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/mentalhealth.png",
+        title: "#mentalhealth",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/Anxiety.png",
+        title: "#anxiety",
+        subtitle: "65,523 Posted &#8226 Trending",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/selfcare.png",
+        title: "#selfcare",
+        subtitle: "51,354 Trending &#8226 in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/mindfullness.png",
+        title: "#mindfulness",
+        subtitle: "48,029 Posted by this tag",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/therapy.png",
+        title: "#therapy",
+        subtitle: "51,354 &#8226 Trending in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/wellbeing.png",
+        title: "#wellbeing",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/mentalhealth.png",
+        title: "#mentalhealth",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/Anxiety.png",
+        title: "#anxiety",
+        subtitle: "65,523 Posted &#8226 Trending",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/selfcare.png",
+        title: "#selfcare",
+        subtitle: "51,354 Trending &#8226 in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/mindfullness.png",
+        title: "#mindfulness",
+        subtitle: "48,029 Posted by this tag",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/therapy.png",
+        title: "#therapy",
+        subtitle: "51,354 &#8226 Trending in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/wellbeing.png",
+        title: "#wellbeing",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/mentalhealth.png",
+        title: "#mentalhealth",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/Anxiety.png",
+        title: "#anxiety",
+        subtitle: "65,523 Posted &#8226 Trending",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/selfcare.png",
+        title: "#selfcare",
+        subtitle: "51,354 Trending &#8226 in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/mindfullness.png",
+        title: "#mindfulness",
+        subtitle: "48,029 Posted by this tag",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/therapy.png",
+        title: "#therapy",
+        subtitle: "51,354 &#8226 Trending in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/wellbeing.png",
+        title: "#wellbeing",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/mentalhealth.png",
+        title: "#mentalhealth",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/Anxiety.png",
+        title: "#anxiety",
+        subtitle: "65,523 Posted &#8226 Trending",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/selfcare.png",
+        title: "#selfcare",
+        subtitle: "51,354 Trending &#8226 in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/mindfullness.png",
+        title: "#mindfulness",
+        subtitle: "48,029 Posted by this tag",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/therapy.png",
+        title: "#therapy",
+        subtitle: "51,354 &#8226 Trending in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/wellbeing.png",
+        title: "#wellbeing",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/mentalhealth.png",
+        title: "#mentalhealth",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    },
+    {
+        image: "/image/WhisperingSoul/Anxiety.png",
+        title: "#anxiety",
+        subtitle: "65,523 Posted &#8226 Trending",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/selfcare.png",
+        title: "#selfcare",
+        subtitle: "51,354 Trending &#8226 in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/mindfullness.png",
+        title: "#mindfulness",
+        subtitle: "48,029 Posted by this tag",
+        color: "#E375BE"
+    },
+    {
+        image: "/image/WhisperingSoul/therapy.png",
+        title: "#therapy",
+        subtitle: "51,354 &#8226 Trending in Indonesia",
+        color: "#DFEFD3"
+    },
+    {
+        image: "/image/WhisperingSoul/wellbeing.png",
+        title: "#wellbeing",
+        subtitle: "82,645 Posted by this tag",
+        color: "#63bbbc"
+    }
+]
+
+let popular_content = document.getElementById("popular-tags-content")
+popular_day.forEach(a => {
+    popular_content.innerHTML += `
+        <div class="popular-class">
+            <div class="popular-image" style="background-color: ${a.color}">
+                <img class="image-mentalhealth" src="${a.image}">
+            </div>
+
+            <div class="popular-text">
+                <h3>${a.title}</h3>
+                <p>${a.subtitle}</p>
+            </div>
+        </div>
+    `
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    const img = document.querySelector(".like-img");
+    img.addEventListener("click", function () {
+        let src = this.src.split("/").pop();
+
+        if (src === "Like-oren.png") {
+            this.src = "../image/WhisperingSoul/bagian-tengah/Like-abu.png";
+        } else {
+            this.src = "../image/WhisperingSoul/bagian-tengah/Like-oren.png";
+        }
+    });
+});
+
+// right container - recommendation
+const recommendation_data = [
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend1-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Clarissa Kosasih"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend2-image.png",
+        title : "The Power of Community in Mental Wellbeing",
+        subtitle : "by Jeffrey K"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend3-image.png",
+        title : "From Struggle to Strength: A Mental Health Journey",
+        subtitle : "by Marcelline Catherine"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend4-image.png",
+        title : "Money, Happiness, and Mental Wellness",
+        subtitle : "by William"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend5-image.png",
+        title : "Prioritizing Mental Health in a Busy World",
+        subtitle : "by Justin H"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend6-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Nathan Otniel"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend1-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Clarissa Kosasih"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend2-image.png",
+        title : "The Power of Community in Mental Wellbeing",
+        subtitle : "by Jeffrey K"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend3-image.png",
+        title : "From Struggle to Strength: A Mental Health Journey",
+        subtitle : "by Marcelline Catherine"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend4-image.png",
+        title : "Money, Happiness, and Mental Wellness",
+        subtitle : "by William"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend5-image.png",
+        title : "Prioritizing Mental Health in a Busy World",
+        subtitle : "by Justin H"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend6-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Nathan Otniel"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend1-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Clarissa Kosasih"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend2-image.png",
+        title : "The Power of Community in Mental Wellbeing",
+        subtitle : "by Jeffrey K"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend3-image.png",
+        title : "From Struggle to Strength: A Mental Health Journey",
+        subtitle : "by Marcelline Catherine"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend4-image.png",
+        title : "Money, Happiness, and Mental Wellness",
+        subtitle : "by William"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend5-image.png",
+        title : "Prioritizing Mental Health in a Busy World",
+        subtitle : "by Justin H"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend6-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Nathan Otniel"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend1-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Clarissa Kosasih"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend2-image.png",
+        title : "The Power of Community in Mental Wellbeing",
+        subtitle : "by Jeffrey K"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend3-image.png",
+        title : "From Struggle to Strength: A Mental Health Journey",
+        subtitle : "by Marcelline Catherine"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend4-image.png",
+        title : "Money, Happiness, and Mental Wellness",
+        subtitle : "by William"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend5-image.png",
+        title : "Prioritizing Mental Health in a Busy World",
+        subtitle : "by Justin H"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend6-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Nathan Otniel"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend1-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Clarissa Kosasih"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend2-image.png",
+        title : "The Power of Community in Mental Wellbeing",
+        subtitle : "by Jeffrey K"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend3-image.png",
+        title : "From Struggle to Strength: A Mental Health Journey",
+        subtitle : "by Marcelline Catherine"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend4-image.png",
+        title : "Money, Happiness, and Mental Wellness",
+        subtitle : "by William"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend5-image.png",
+        title : "Prioritizing Mental Health in a Busy World",
+        subtitle : "by Justin H"
+    },
+    {
+        image : "/image/WhisperingSoul/support-recommend/recommend6-image.png",
+        title : "Overcoming Burnout & Finding Balance",
+        subtitle : "by Nathan Otniel"
+    }
+]
+
+let recommendation = document.getElementById("many-content")
+
+recommendation_data.forEach(a => {
+    recommendation.innerHTML += `
+        <div id="recommended-content">
+            <img class="recommend-image" src="${a.image}">
+            <div class="recommend-info">
+                <p class="recommend-judul">${a.title}</p>
+                <p class="creator">${a.subtitle}</p>
+            </div>
+            <img class="panah-kanan-abu" src="/image/WhisperingSoul/support-recommend/panan-kanan-abu.png">
+        </div>
+    `
+})
+
+document.querySelector('#a').addEventListener("click", () => {
+  window.location.href = "../WhisperingSoul/whisperingSoull.html"
+})
