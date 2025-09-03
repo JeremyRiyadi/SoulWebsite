dedbb3e15f6d2e27c3b140eba2c7aef126f7b4fa
Jeremy Djohar Riyadi <162307355+JeremyRiyadi@users.noreply.github.com>
2025-09-03 15:51:39 +0700

    Add files via upload

---
+++ SoulSupportMain/script.js
@@ -0,0 +1,7 @@
+document.querySelector('#back-button').addEventListener("click", () => {
+    window.location.href = "../MainPage/mainpage.html"
+})
+
+document.querySelector('.book-session').addEventListener("click", () => {
+  window.location.href = "../SoulSupport-Psychologist/psychologyst.html"
+})
