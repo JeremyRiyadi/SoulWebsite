596611e1bf8b36ea8c7384e8f262247b88d28330
Jeremy Djohar Riyadi <162307355+JeremyRiyadi@users.noreply.github.com>
2025-09-03 15:44:46 +0700

    Add files via upload

---
+++ SereneReminderEdit/serene-reminder.js
@@ -0,0 +1,7 @@
+document.querySelector('.back').addEventListener("click", () => {
+  window.location.href = "../SereneRemindersMain/index.html"
+})
+
+document.querySelector('.save-btn').addEventListener("click", () => {
+  window.location.href = "../SereneRemindersMain/index.html"
+})
